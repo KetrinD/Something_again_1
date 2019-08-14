474cfd7dbed0e1810bbec73c2e3fcfd7a76e2b9b
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2019-08-15 01:11:14 +0300

    23

---
+++ something/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace something
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Something smth = new Something(3,8);
+            int r = smth.Multiply();
+            Console.WriteLine($"{r}");
+
+            Console.WriteLine("{type X:}");
+            int qa = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{type Y:}");
+            int az = Convert.ToInt32(Console.ReadLine());
+
+            Something smth_2 = new Something(qa, az);
+            int ert = smth_2.Multiply();
+            Console.WriteLine($"{ert}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ something/Something.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace something
+{
+    public class Something
+    {
+        public int X {get;set;}
+        public int Y { get; set; }
+
+        public Something()
+        {
+        }
+
+        public Something(int n, int m)
+        {
+            X = n;
+            Y = m;
+        }
+        public int Multiply()
+        {
+            return X + Y;
+        }
+    }
+}
